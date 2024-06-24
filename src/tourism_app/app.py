d740acd4987e2e3fd9753874315ddc4c2556ce88
Lucas28reis <crreacicomp@gmail.com>
2024-06-24 00:47:31 +0000

    💄 Update

--- src/tourism_app/app.py
+++ src/tourism_app/app.py
@@ -1,7 +1,30 @@
 import streamlit as st 
+from agent import Agent
 
-st.write(
-    """
-    # Tourism Agent App
-    This app predicts the **Tourism**!
+agent = Agent("")
+
+st.set_page_config('wide')
+st.title('Tourism intellAgent App')
+
+st.write("""
+    This app predicts the **Tourism**!         
 """)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    request = st.text_area("Onde você gostaria de ir?")
+    button = st.button("Pedir sugestão de roteiro")
+    box = st.container(height=300)
+    with box:
+        container = st.empty()
+        container.header("Itinerário")
+if button and request:
+    itinerary = agent.get_itinerary(request)
+    try:
+        container.write(itinerary['agent_suggestion'] + 
+                        "<br> Coordenadas:" + 
+                        itinerary['coordinates'])
+
+    except KeyError:
+        container.write("Desculpe, não consegui encontrar um roteiro para você.")
